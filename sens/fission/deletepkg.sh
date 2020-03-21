0a7d3d293e3fc0cb4476c56da65f52600e173f4b
Mayank <mayank@dozee.io>
2020-03-21 17:58:00 +0530

    Create deletepkg.sh

---
+++ sens/fission/deletepkg.sh
@@ -0,0 +1,14 @@
+for f in `fission fn list | awk '{print $1}'`
+do
+  if [ "$f" == "NAME" ]; then
+    continue
+  fi
+  fission fn delete --name $f --verbosity=2
+done
+for f in `fission pkg list | awk '{print $1}'`
+do
+  if [ "$f" == "NAME" ]; then
+    continue
+  fi
+  fission pkg delete --name $f --verbosity=2
+done
