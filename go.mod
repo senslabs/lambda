a2fda00eebf85e2d514ffe1aefcb39837a310115
Mayank <mayank@dozee.io>
2020-03-21 14:12:40 +0530

    Adding device apis

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/google/uuid v1.1.1
-	github.com/senslabs/alpha v0.0.0-20200319092848-8c52cc956ee3
+	github.com/gorilla/mux v1.7.4
+	github.com/senslabs/alpha v0.0.0-20200321052844-f8bbfd688911
 )
---
+++ sens/fission/src/device/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/senslabs/alpha/sens/errors"
+	"github.com/senslabs/alpha/sens/httpclient"
+	"github.com/senslabs/alpha/sens/logger"
+	"github.com/senslabs/lambda/sens/fission/response"
+)
+
+func getDatastoreUrl() string {
+	baseUrl := os.Getenv("DATASTORE_BASE_URL")
+	if baseUrl == "" {
+		return "http://datastore.senslabs.me"
+	}
+	return baseUrl
+}
+
+func duplicateDevice(w http.ResponseWriter, r *http.Request, orgId string, userId string, status string) error {
+	logger.InitLogger("")
+
+	if orgId == "" && userId == "" && status == "" {
+		return httpclient.WriteError(w, http.StatusBadRequest, errors.New(http.StatusBadRequest, "No change in data"))
+	}
+	deviceId := mux.Vars(r)["deviceId"]
+	var device map[string]interface{}
+	url := fmt.Sprintf("%s%s", getDatastoreUrl(), "/api/devices/find")
+	and := httpclient.HttpParams{"DeviceId": {deviceId}}
+	code, err := httpclient.Get(url, and, nil, &device)
+	logger.Debugf("%d, %#v", code, device)
+	if err != nil {
+		return httpclient.WriteError(w, code, err)
+	} else {
+		delete(device, "Id")
+		device["CreatedAt"] = time.Now()
+		if status != "" {
+			device["Status"] = status
+		}
+		if orgId != "" {
+			device["OrgId"] = orgId
+		}
+		if userId != "" {
+			device["UserId"] = userId
+		}
+		url := fmt.Sprintf("%s%s", getDatastoreUrl(), "/api/devices/create")
+		code, _, err := httpclient.PostR(url, and, nil, &device)
+		if err != nil {
+			return httpclient.WriteError(w, code, err)
+		}
+	}
+	return nil
+}
+
+func CreateDevice(w http.ResponseWriter, r *http.Request) {
+	logger.InitLogger("sens.lambda.CreateDevice")
+	url := fmt.Sprintf("%s%s", getDatastoreUrl(), "/api/devices/create")
+	code, data, err := httpclient.PostR(url, nil, nil, r.Body)
+	logger.Debug(code, string(data))
+	if err != nil {
+		logger.Error(err)
+		response.WriteError(w, code, err)
+	} else {
+		fmt.Fprintln(w, string(data))
+	}
+}
+
+type DeviceUpdateBody struct {
+	DeviceId string
+	OrgId    string
+	UserId   string
+}
+
+const (
+	REGISTERED   = "REGISTERED"
+	UNREGISTERED = "UNREGISTERED"
+	PAIRED       = "PAIRED"
+	UNPAIRED     = "UNPAIRED"
+)
+
+func RegisterDevice(w http.ResponseWriter, r *http.Request) {
+	logger.InitLogger("sens.lambda.RegisterDevice")
+	var body DeviceUpdateBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		httpclient.WriteError(w, http.StatusInternalServerError, err)
+	} else {
+		duplicateDevice(w, r, body.OrgId, body.UserId, REGISTERED)
+	}
+}
+
+func UnregisterDevice(w http.ResponseWriter, r *http.Request) {
+	logger.InitLogger("sens.lambda.UnregisterDevice")
+	duplicateDevice(w, r, "", "", UNREGISTERED)
+}
+
+func PairDevice(w http.ResponseWriter, r *http.Request) {
+	logger.InitLogger("sens.lambda.PairDevice")
+	var body DeviceUpdateBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		httpclient.WriteError(w, http.StatusInternalServerError, err)
+	} else {
+		duplicateDevice(w, r, body.OrgId, body.UserId, PAIRED)
+	}
+}
+
+func UnpairDevice(w http.ResponseWriter, r *http.Request) {
+	logger.InitLogger("sens.lambda.UnpairDevice")
+	var body DeviceUpdateBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		httpclient.WriteError(w, http.StatusInternalServerError, err)
+	} else {
+		duplicateDevice(w, r, body.OrgId, body.UserId, UNPAIRED)
+	}
+}
+
+func main() {}
